d524adc76b9e7939002bfc337ec67fb2a85924ad
Richard Thoya <rkthoya@gmail.com>
2022-02-19 15:47:46 +0300

    Bootstrap and crispy forms

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
     'accounts',
+    'pages',
+
+    # 3rd Party
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +131,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

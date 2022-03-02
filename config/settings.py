8c087722f8974cd14a93a0fc80828969931dd530
Richard Thoya <rkthoya@gmail.com>
2022-03-02 11:03:09 +0300

    Newspaper app.

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # Local
     'accounts',
     'pages',
+    'articles',
 
     # 3rd Party
     'crispy_forms',
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
